c495088e5a9b2748dc49037e8ad5c7cdfec05de3
Roger Teletor <RogerTeletor@gmail.com>
2024-10-17 19:18:43 +0200

    forgot to add true in a variable in js

--- button.js
+++ button.js
@@ -12,7 +12,7 @@ let parent = document.body;
 parent.appendChild(father);
 parent.appendChild(button)
 
-let change;
+let change = true;
 const changeColor = () => {
      change ? father.style.backgroundColor = "red" : father.style.backgroundColor = "rgb(12, 21, 21)";
   change = !change; 
