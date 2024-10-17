8cce66d75c48c1fa9bd2bbdeba5ecc76767c1649
Roger Teletor <RogerTeletor@gmail.com>
2024-10-17 19:15:43 +0200

    made the button go back to it's original color in js

--- button.js
+++ button.js
@@ -12,10 +12,10 @@ let parent = document.body;
 parent.appendChild(father);
 parent.appendChild(button)
 
-
+let change;
 const changeColor = () => {
-   
- father.style.backgroundColor = "red";
+     change ? father.style.backgroundColor = "red" : father.style.backgroundColor = "rgb(12, 21, 21)";
+  change = !change; 
 }
 
 button.onclick = changeColor;
